ebeb014151916e06d2ec9324d235e5ac7899624d
Shubh <shubhjhawar78@gmail.com>
2023-07-03 20:02:51 -0400

    worked on the VideoCard Component

--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
-import sanityClient from '@sanity/client';
+import {createClient} from '@sanity/client';
 
-export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+export const client = createClient({
+  projectId: 's0peo8bi',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
