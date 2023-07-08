fd392325501bd23b8d29f289dbd2151117b391a2
Shubh <shubhjhawar78@gmail.com>
2023-07-08 13:46:05 -0400

    search implemented successfully

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/legacy/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -6,7 +6,7 @@ import {GoogleLogin, googleLogout} from '@react-oauth/google';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
-import Logo from '../utils/tiktik-logo.png';
+import Logo from '../utils/logo.png';
 import { createOrGetUser } from '@/utils';
 import useAuthStore from '@/store/authStore';
 
@@ -16,6 +16,17 @@ const Navbar = () => {
 
   // 6. call the the two things created through ZUSTAND 
   const {userProfile, addUser, removeUser}: any = useAuthStore();
+  const [searchValue, setSearchValue] = useState('');
+  const router = useRouter();
+
+  const handleSearch = (e: {
+    preventDefault: () => void}) => {
+    e.preventDefault(); // prevent reloading
+
+    if(searchValue){
+      router.push(`/search/${searchValue}`)
+    }
+  }
 
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -30,7 +41,24 @@ const Navbar = () => {
       </div>
       </Link>
 
-      <div>SEARCH FOR</div>
+      <div className='relative hidden md:block'>
+        <form className='absolute md:static top-10 -left-20 bg-white'
+          onSubmit={handleSearch}
+        >
+          <input type='text'
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                placeholder='Search accounts or videos'
+                className='bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:top-0'
+          />
+          <button
+            onClick ={handleSearch}
+            className='absolute md:right-5 right-6 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400 mt-4'
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
 
       <div>
         {userProfile ? (
